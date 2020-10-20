901224a26f626c7a7baba75aff5c1a5407ce1fb8
Andrea Gotelli <gotelliandrea@gmail.com>
2020-10-20 09:09:12 +0200

    Creating a package for simulating the robot

---
+++ robot_simulation/src/robot.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
+#include "robot_simulation_messages/MoveTo.h"
+
+bool move(robot_simulation_messages::MoveToRequest&,
+          robot_simulation_messages::MoveToResponse&)
+{
+  ROS_INFO_STREAM("calling the server");
+  ros::Duration waiting_time(3);
+  waiting_time.sleep();
+  return true;
+}
+
+//void move1(const geometry_msgs::Pose::ConstPtr target)
+//{
+//  ros::Duration waiting_time(3);
+//  waiting_time.sleep();
+//}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "robot");
+  ros::NodeHandle nh;
+
+  ros::ServiceServer reach_position = nh.advertiseService("/MoveToPosition", move);
+//  ros::Subscriber receivedTarget = nh.subscribe<geometry_msgs::Pose>("/MoveToPosition", 10, move1);
+
+  ros::Rate frame_rate = ros::Rate(50.0);
+
+  while(ros::ok()) {
+    ros::spinOnce();
+
+    frame_rate.sleep();
+  }
+}
+
